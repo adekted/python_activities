b1be1cebcd35bb7f73f483e056af578d743243bc
adekted <aymercad@uci.edu>
2018-05-05 11:38:13 -0700

    Making changes to dictionaries.py

--- python_activity2/dictionaries.py
+++ python_activity2/dictionaries.py
@@ -3,4 +3,25 @@ lst = ["A","B","C"]
 item = {"name:":"Adrian","status":"hungry","favorite food":"McFlurries","bag":["laptop","usb","food"]}
 reddit = {"reddit" : {"key" : [1,2,3,4]}}
 
+'''
 print(reddit["reddit"]["key"][1])
+
+for keys in item:
+    print(keys)
+'''
+
+adrian = {
+    "name":"Adrian Mercado",
+    "age":22,
+    "hobbies":["powerlifting","raving","eating ice cream"],
+    "wake_up":{"Monday":"9:00 AM","Tuesday":"1:00 PM", "Wednesday":"11:00 AM"}
+}
+
+print(adrian["name"])
+print(str(len(adrian["hobbies"])) + " hobbies")
+for hobby in adrian["hobbies"]:
+    print(hobby)
+for times in adrian["wake_up"]:
+    print(times + " " +str(adrian["wake_up"][times]))
+for k,v in adrian["wake_up"].items():
+    print(k,v)
