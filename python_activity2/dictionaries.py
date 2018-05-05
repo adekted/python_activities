9be9f360718a5b6762a29a4722eabf78c5da03c4
adekted <aymercad@uci.edu>
2018-05-05 11:14:15 -0700

    Adding the dictionaries.py

---
+++ python_activity2/dictionaries.py
@@ -0,0 +1,6 @@
+lst = ["A","B","C"]
+
+item = {"name:":"Adrian","status":"hungry","favorite food":"McFlurries","bag":["laptop","usb","food"]}
+reddit = {"reddit" : {"key" : [1,2,3,4]}}
+
+print(reddit["reddit"]["key"][1])
